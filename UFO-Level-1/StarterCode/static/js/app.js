eb6bdcb529abba05832a92a05e42c7a618ade9b2
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-07 23:45:54 -0500

    Button

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -8,6 +8,13 @@ console.log(tableData);
 // write code that appends a table to your web page and 
 // then adds new rows of data for each UFO sighting.
 
+// Make sure you have a column for `date/time`, `city`, 
+// `state`, `country`, `shape`, and `comment` at the very least.
+
+// Use a date form in your HTML document and write JavaScript code 
+// that will listen for events and search through the `date/time` 
+// column to find rows that match user input.
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
@@ -25,18 +32,34 @@ tableData.forEach((ufoSightings) => {
     });
 });
 
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
 
 
 
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("datetime");
 
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
 
-// Make sure you have a column for `date/time`, `city`, 
-// `state`, `country`, `shape`, and `comment` at the very least.
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter((sighting) => sighting.datetime === inputValue);
+    console.log(filteredData);
 
-// Use a date form in your HTML document and write JavaScript code 
-// that will listen for events and search through the `date/time` 
-// column to find rows that match user input.
+    // Adding tag
+    tbody.html("");
+}
 
 
 
