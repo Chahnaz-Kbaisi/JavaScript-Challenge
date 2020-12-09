20eb42c505c6610de1c9fc28cd520dbb7d10b711
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-08 19:04:34 -0500

    Setting Advanced Panel

--- UFO-Level-2/StarterCode/static/js/app.js
+++ UFO-Level-2/StarterCode/static/js/app.js
@@ -1,4 +1,26 @@
+// Level 2: Multiple Search Categories (Optional)
+
+// Complete all of Level 1 criteria.
+
+// Using multiple `input` tags and/or select dropdowns, 
+// write JavaScript code so the user can to set multiple filters 
+// and search for UFO sightings using the following criteria based on the table columns:
+
+//   1. `date/time`
+//   2. `city`
+//   3. `state`
+//   4. `country`
+//   5. `shape`
+
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Data columns
+var columns = ["datetime", "city", "state", "shape", "durationMinutes", "comments"]
+
+
+// Advanced Search Panel
